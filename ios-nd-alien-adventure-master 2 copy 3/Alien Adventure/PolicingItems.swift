53976f9559a1d2c2783cf238748a4077d9fb0f9a
daxapps <daxapps777@gmail.com>
2016-08-31 10:32:44 -0500

    Policing Items

--- ios-nd-alien-adventure-master 2 copy 3/Alien Adventure/PolicingItems.swift
+++ ios-nd-alien-adventure-master 2 copy 3/Alien Adventure/PolicingItems.swift
@@ -9,8 +9,25 @@
 extension Hero {
     
     func policingItems(inventory: [UDItem], policingFilter: UDItem throws -> Void) -> [UDPolicingError:Int] {
-        return [UDPolicingError:Int]()
-    }    
+        var errors = [UDPolicingError:Int]()
+        errors[.ItemFromCunia] = 0
+        errors[.NameContainsLaser] = 0
+        errors[.ValueLessThan10] = 0
+        
+        for item in inventory {
+            do {
+                try policingFilter(item)
+                } catch UDPolicingError.ItemFromCunia {
+                    errors[.ItemFromCunia]! += 1
+                } catch UDPolicingError.NameContainsLaser {
+                    errors[.NameContainsLaser]! += 1
+                } catch UDPolicingError.ValueLessThan10 {
+                    errors[.ValueLessThan10]! += 1
+                } catch {
+                    print("some errors")
+                }
+        }
+        return errors
+    }
 }
-
 // If you have completed this function and it is working correctly, feel free to skip this part of the adventure by opening the "Under the Hood" folder, and making the following change in Settings.swift: "static var RequestsToSkip = 1"
