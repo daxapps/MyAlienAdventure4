605829b8ed838f422beb899b21a0038db141559c
daxapps <daxapps777@gmail.com>
2016-09-01 10:17:31 -0500

    GetCommonItems

--- ios-nd-alien-adventure-master 2 copy 3/Alien Adventure/GetCommonItems.swift
+++ ios-nd-alien-adventure-master 2 copy 3/Alien Adventure/GetCommonItems.swift
@@ -9,7 +9,7 @@
 extension Hero {
     
     func getCommonItems(inventory: [UDItem]) -> [UDItem] {
-        return [UDItem]()
+        return inventory.filter({(item: UDItem) -> Bool in return item.rarity.rawValue == UDItemRarity.Common.rawValue })
     }
     
 }
